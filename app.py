0529d6c1f0df18f1c704b30844cba77a43767d09
ndoubleu <65906953+ndoubleu@users.noreply.github.com>
2022-03-21 19:31:49 +0500

    First release

---
+++ app.py
@@ -0,0 +1,92 @@
+
+import os
+import handlers
+from aiogram import executor, types
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from data import config
+from loader import dp, db, bot
+import filters
+import logging
+
+filters.setup(dp)
+
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.environ.get("PORT", 5000))
+user_message = 'Пользователь'
+admin_message = 'Админ'
+
+
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    cid = message.chat.id
+    if cid in config.ADMINS:
+        config.ADMINS.remove(cid)
+    await message.answer('''Привет! 👋
+T•Bone Food
+Ресторан быстрого питания
+🍔 Самые сочные бургеры
+🍕 Итальянская пицца
+📍Ул. Гагарина, дом 49
+📌Ориентир завод "Кинап" 
+📞 +99888 207 1111
+📞 +99897 392 2020
+🛍️ Чтобы перейти в каталог и выбрать приглянувшиеся товары возпользуйтесь командой /menu.
+    ''', reply_markup=markup)
+
+
+@dp.message_handler(text='TboneUserSettings')
+async def user_mode(message: types.Message):
+
+    cid = message.chat.id
+    if cid in config.ADMINS:
+        config.ADMINS.remove(cid)
+
+    await message.answer('Включен пользовательский режим.', reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(text='TboneAdminSettings')
+async def admin_mode(message: types.Message):
+
+    cid = message.chat.id
+    if cid not in config.ADMINS:
+        config.ADMINS.append(cid)
+
+    await message.answer('Включен админский режим.', reply_markup=ReplyKeyboardRemove())
+
+
+async def on_startup(dp):
+    logging.basicConfig(level=logging.INFO)
+    db.create_tables()
+
+    await bot.delete_webhook()
+    await bot.set_webhook(config.WEBHOOK_URL)
+
+
+async def on_shutdown():
+    logging.warning("Shutting down..")
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+    logging.warning("Bot down")
+
+
+if __name__ == '__main__':
+
+    if "HEROKU" in list(os.environ.keys()):
+
+        executor.start_webhook(
+            dispatcher=dp,
+            webhook_path=config.WEBHOOK_PATH,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+        )
+
+    else:
+
+        executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
---
+++ keyboards/default/markups.py
@@ -0,0 +1,37 @@
+from aiogram.types import ReplyKeyboardMarkup
+
+back_message = '👈 Назад'
+confirm_message = '✅ Подтвердить заказ'
+all_right_message = '✅ Все верно'
+cancel_message = '🚫 Отменить'
+location = '📍Текущая локация'
+
+def confirm_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(confirm_message)
+    markup.add(back_message)
+
+    return markup
+
+def back_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(back_message)
+
+    return markup
+def back_location_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(location)
+    markup.add(back_message)
+
+    return markup
+def check_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(back_message, all_right_message)
+
+    return markup
+
+def submit_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(cancel_message, all_right_message)
+
+    return markup
